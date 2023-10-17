45accff1f2889ee942af0c37a37f750507496be9
Pritam <pritammuli@gmail.com>
2023-10-17 13:00:38 +0530

    updated Changes

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const stripe = require("stripe")(
 // checkout api
 connectToMongoose();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // app.use(cors());
 app.use(express.json());
